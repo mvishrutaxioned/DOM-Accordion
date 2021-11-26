81477f8ec75b57b124770ece649fa6d1343af3de
VishrutMargam <vishrutm@axioned.com>
2021-11-26 14:02:47 +0530

    added recommended changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
 $(document).ready(() => {
-    console.log('ready')
+
+    var openSlide = -1;
 
     $('.accordion li').each(function(index, acc) {
         $(this).click(function(e){
             e.preventDefault();
+            
+            $(this).find('div').slideToggle()
+            $(this).find('h2').toggleClass('show')
 
-            $('.accordion li div').each(function(num, li) {
-                $(this).slideUp()
-                if(num == index) {
-                    $(this).slideToggle()
-                }
-            })
+            if(openSlide > -1) {
+                var list = $('.accordion li').eq(openSlide)
+                list.find('div').slideUp();
+                if(list.find('h2').hasClass('show')) list.find('h2').removeClass('show')
+            }
+            openSlide = index
         })
     })
 })
